5f4b5071411621806d1442440d3b1aadb925cb43
Kamil Błoński <kblonski02@gmail.com>
2024-11-19 23:42:05 +0100

    Push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,26 @@
     <div>
         <h1>Home View</h1>
         {{ data }}
+        {{ loading }}
     </div>
 </template>
 
 <script>
-export default {
-    setup(){
-        const reader = new FileReader();
-        read.cs
+import { watch } from 'vue';
+import { useSimcLocations } from '../hooks/simcLocations';
 
+export default { 
+    setup(){
+        const simcAdd = useSimcLocations()
+        watch( simcAdd.loading, () => {
+            console.log( simcAdd.loading)
+        })
+
+        return {
+            data: simcAdd.data,
+            loading: simcAdd.loading,
+            errors: simcAdd.error
+        }
     }
 }
 </script>
