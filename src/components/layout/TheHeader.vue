0e218e218fa6eee1314dae296b2dab4c2ed11533
Kamil Błoński <kblonski02@gmail.com>
2024-11-20 21:06:03 +0100

    Add page header

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ appName }}</h1>
+    </div>
+</template>
+
+<script>
+import { appName } from '../../config.js';
+
+export default {
+    data(){
+        return {
+            appName
+        }
+    }
+}
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,30 @@
 <template>
     <div>
-        <h1>Home View</h1>
-        {{ data }}
-        {{ loading }}
+        <h2>Home View</h2>
     </div>
 </template>
 
 <script>
-import { watch } from 'vue';
+import { watch, ref, computed } from 'vue';
 import { useSimcLocations } from "../hooks/simcLocations";
 
 export default { 
     setup(){
         const simcLocations = useSimcLocations()
-        watch( [simcLocations.data], () => {
-            console.log('test')
-            console.log( simcLocations.loading)
+        const loading = computed(() => simcLocations.loading.value)
+        const errors = computed( () => simcLocations.error.value)
+        const locations = ref([])
+
+        watch( [simcLocations.data, simcLocations.loading, simcLocations.error], () => {
+            if (loading.value) return;
+            if (errors.value) return;
+
+            locations.value = simcLocations.data.value.map( lineList => lineList[6])
+
         })
  
         return {
-            data: simcLocations.data,
+            data: [],
             loading: simcLocations.loading,
             errors: simcLocations.error
         }
