25792d46885b5bb267a33ff53b7be0ffc546aa89
Kamil Błoński <kblonski02@gmail.com>
2024-11-27 20:17:34 +0100

    Add function draw curve chart

---
+++ src/utils/drawCurveChart.js
@@ -0,0 +1,22 @@
+export function drawChart(
+    {
+        data = [],
+        title = '# Chart Title',
+        elementId = null
+
+    }
+) {
+    var data = google.visualization.arrayToDataTable(data);
+
+    var options = {
+        title,
+        curveType: "function",
+        legend: { position: "top" },
+    };
+
+    var chart = new google.visualization.LineChart(
+        document.getElementById(elementId)
+    );
+
+    chart.draw(data, options);
+}
