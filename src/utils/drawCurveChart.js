463b27fb843943b0e2d2605c0faedb9770c3da70
Kamil Błoński <kblonski02@gmail.com>
2024-11-28 06:15:33 +0100

    Innstall googlechart package in vue repository

--- src/utils/drawCurveChart.js
+++ src/utils/drawCurveChart.js
@@ -1,3 +1,6 @@
+import { GoogleCharts } from "google-charts";
+
+
 export function drawChart(
     {
         data = [],
@@ -6,17 +9,30 @@ export function drawChart(
 
     }
 ) {
-    var data = google.visualization.arrayToDataTable(data);
-    
-    var options = {
-        title,
-        curveType: "function",
-        legend: { position: "top" },
-    };
-    
-    var chart = new google.visualization.LineChart(
-        document.getElementById(elementId)
-    );
+    if (!elementId) {
+        console.error("Element ID is required to render the chart.");
+        return;
+    }
 
-    chart.draw(data, options);
+    if (!data.length) {
+        console.error("Data is required to render the chart.");
+        return;
+    }
+
+    GoogleCharts.load(() => {
+
+        const chartData = GoogleCharts.api.visualization.arrayToDataTable(data);
+        
+        const options = {
+            title,
+            curveType: "function",
+            legend: { position: "top" },
+        };
+        
+        const chart = new GoogleCharts.api.visualization.LineChart(
+            document.getElementById(elementId)
+        );
+    
+        chart.draw(chartData, options);
+    })
 }
