5fbd8103efbf0a00f85b51661ca7059e2cd060e6
Kamil Błoński <kblonski02@gmail.com>
2024-11-28 06:49:42 +0100

    Add basemodal to project

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import store from './storage'
 const BaseLoading = defineAsyncComponent( ()=> import("./components/UI/BaseLoading.vue"))
 const BaseSearch = defineAsyncComponent( () => import('./components/UI/BaseSearch.vue'))
 const BaseDropdown = defineAsyncComponent( () => import('./components/UI/BaseDropdown.vue'))
+const BaseModal = defineAsyncComponent( () => import('./components/UI/BaseModal.vue'))
 
 const app = createApp(App)
 
 app.component('base-loading', BaseLoading)
 app.component('base-search', BaseSearch)
 app.component('base-dropdown', BaseDropdown)
+app.component('base-modal', BaseModal)
 app.use( router )
 app.use( store )
 
