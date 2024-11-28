550bea84c2f5f1574aee4a24740f31466575b122
Kamil Błoński <kblonski02@gmail.com>
2024-11-28 01:13:55 +0100

    Add base dropdown component

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from './storage'
 
 const BaseLoading = defineAsyncComponent( ()=> import("./components/UI/BaseLoading.vue"))
 const BaseSearch = defineAsyncComponent( () => import('./components/UI/BaseSearch.vue'))
+const BaseDropdown = defineAsyncComponent( () => import('./components/UI/BaseDropdown.vue'))
 
 const app = createApp(App)
 
 app.component('base-loading', BaseLoading)
 app.component('base-search', BaseSearch)
+app.component('base-dropdown', BaseDropdown)
 app.use( router )
 app.use( store )
 
